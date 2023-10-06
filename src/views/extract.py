46687ea2ce44bd5566ec852f0fd8650bf5a08295
rachhtord <rachael.tordoff@hotmail.co.uk>
2023-10-06 09:14:22 +0100

    added sqs and unit tests

--- src/views/extract.py
+++ src/views/extract.py
@@ -5,10 +5,7 @@ from flask import (
     jsonify
 )
 from src.utilities import jwt_util
-from src.utilities.web_scrape import WebScraper
-
-
-from src.dependencies.openapi import DataExtractor
+from src.dependencies import sqs
 
 extract = Blueprint("extract", __name__)
 
@@ -29,8 +26,6 @@ def document_upload():
 
     phrases_list = request.form.getlist('phrases[]')
     return jsonify(phrases_list)
-    data_extractor = DataExtractor()
-    data_extractor.extract_data_from_bank_statement(post_data)
 
     return render_template(
         "pages/extract-home.html"
@@ -44,17 +39,15 @@ def url_list():
     post_data = request.form
     phrases_list = request.form.getlist('phrases[]')
     url_list = post_data.get('urls', '').split("\r\n")
-    web_scraper_instance = WebScraper()
 
-    scraped_websites = web_scraper_instance.site_scrape(url_list)
     new_dict = {
         "scraped_websites": scraped_websites,
         "phrases_list": phrases_list,
-        "output_typeurl": post_data.get('output_typeurl')
+        "output_typeurl": post_data.get('output_typeurl'),
+        "type": 'urls'
     }
-    data_extractor = DataExtractor()
+    sqs.send_create_doc_data(new_dict)
 
-    data_extractor.extract_data_from_webscraped_urls(new_dict)
     return render_template(
         "pages/extract-home.html"
     )
@@ -66,9 +59,6 @@ def extract_pdf():
 
     post_data = request.form
     return jsonify(post_data)
-    data_extractor = DataExtractor()
-
-    data_extractor.extract_data_from_bank_statement(post_data)
 
     return render_template(
         "pages/extract-home.html"
